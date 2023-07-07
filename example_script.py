8e6e1d21ff5886a3f8974e21f8a37d0cf53e8aef
Cliff Kerr <questionably@gmail.com>
2023-07-07 09:45:06 +0300

    Change to use normally distributed random number

--- example_script.py
+++ example_script.py
@@ -3,4 +3,5 @@ Example file for GitHub usage
 '''
 
 import numpy as np
-print('Here is a random number', np.random.rand())
+print('Here is a normally distributed'
+      'random number', np.random.randn())
